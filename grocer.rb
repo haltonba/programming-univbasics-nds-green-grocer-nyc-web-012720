6996cc4b38203c72fc9e3bae7a8937968bbb3f03
Halton Bagley <haltonba94@gmail.com>
2020-01-08 22:31:43 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -31,7 +31,7 @@ def apply_coupons(cart, coupons)
   while coupon_counter < coupons.length do
     cart_checker = find_item_by_name_in_collection(coupons[coupon_counter][:item], cart)
     coupon_item_name = "#{coupons[coupon_counter][:item]} W/COUPON"
-    couponed_item_name_checker = 
+    couponed_item_name_checker = find_item_by_name_in_collection(coupon_item_name, )
     
     coupon_counter += 1
   end
