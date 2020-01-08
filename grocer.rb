b62b7ee049168abd2ddb61bf7ccb8e65a32c146d
Halton Bagley <haltonba94@gmail.com>
2020-01-08 22:13:43 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,26 +1,53 @@
 def find_item_by_name_in_collection(name, collection)
-  # Implement me first!
-  #
-  # Consult README for inputs and outputs
+  item_counter = 0
+  while item_counter < collection.length do
+    if name === collection[item_counter][:item]
+      return collection[item_counter]
+    end
+    item_counter += 1
+  end
+  nil
 end
 
 def consolidate_cart(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This returns a new Array that represents the cart. Don't merely
-  # change `cart` (i.e. mutate) it. It's easier to return a new thing.
+  new_cart = []
+  item_counter = 0
+  while item_counter < cart.length do
+    new_cart_item = find_item_by_name_in_collection(cart[item_counter][:item], new_cart)
+    if new_cart_item
+      new_cart_item[:count] += 1
+    else
+      new_cart_item = cart[item_counter]
+      new_cart_item[:count] = 1
+      new_cart << new_cart_item
+    end
+    item_counter += 1
+  end
+  new_cart
 end
 
 def apply_coupons(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  coupon_counter = 0
+  while coupon_counter < coupons.length do
+    cart_checker = find_item_by_name_in_collection(coupons[coupon_counter][:item], cart)
+    couponed_item_name = "#{coupons[coupon_counter][:item]} W/COUPON"
+    
+    coupon_counter += 1
+  end
+  cart
 end
 
 def apply_clearance(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  counter = 0
+  unique_cart = consolidate_cart(cart)
+  while counter < unique_cart.length do
+    if unique_cart[counter][:clearance] === true
+      unique_cart[counter][:price] *= 0.8
+      unique_cart[counter][:price].round(2)
+    end
+    counter += 1
+  end
+  unique_cart
 end
 
 def checkout(cart, coupons)
