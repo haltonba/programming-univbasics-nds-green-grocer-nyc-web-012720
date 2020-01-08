a4a94cf7e3a96aae531f2507d6eb98ec233b9900
Halton Bagley <haltonba94@gmail.com>
2020-01-08 22:35:54 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -39,7 +39,7 @@ def apply_coupons(cart, coupons)
       else
         couponed_item_name_checker = {
           item: coupon_item_name,
-          price: 
+          price: coupons[coupon_counter][:cost] /
         }
       end
     end
